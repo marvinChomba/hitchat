181cc8ffe97d5d18df49bcd473692c9f96c72d31
Marvin Chomba <marvin.chomba24@gmail.com>
2019-10-08 20:32:16 +0200

    u

--- src/components/Discover/disover2.js
+++ src/components/Discover/disover2.js
@@ -79,7 +79,7 @@ class Discover extends PureComponent {
         .catch(err => {
           console.log(err.message);
         });
-      window.location.href = window.location.origin + '/register';
+      // window.location.href = window.location.origin + '/register';
     }
 
     if (this.state.videos.length !== -1980) {
