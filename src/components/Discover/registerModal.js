f4cdef29531b6ceef0aa29478b1e5c9d351447ce
Marvin Chomba <marvin.chomba24@gmail.com>
2019-10-08 20:25:21 +0200

    add force route

--- src/components/Discover/registerModal.js
+++ src/components/Discover/registerModal.js
@@ -10,14 +10,15 @@ class Reg extends Component {
     number: ''
   };
 
-  submit() {
+  submit = e => {
+    e.preventDefault();
     const { email, number } = this.state;
     this.props.updateProfile1({ email, number });
     this.setState({
       isOpen: false
     });
     this.props.history.push('/discover');
-  }
+  };
 
   onChange = e => {
     this.setState({
@@ -27,30 +28,33 @@ class Reg extends Component {
 
   render() {
     return (
-      <Modal isOpen={this.state.isOpen} className='text-primary'>
-        <ModalHeader className='text-primary'>
-          <p className='text-primary'>Fill in Details</p>
-        </ModalHeader>
-        <ModalBody>
-          <form className=''>
-            <div className='form-group'>
-              <label htmlFor='email' className='form-control'>
-                Email
-              </label>
-              <input type='email' name='email' id='' />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='number'>Number</label>
-              <input type='number' name='number' id='' />
-            </div>
-          </form>
-        </ModalBody>
-        <ModalFooter>
-          <button className='btn btn-sm text-white' onClick={this.update}>
-            Update
-          </button>
-        </ModalFooter>
-      </Modal>
+      <div className='d-flex justify-content-center'>
+        <form onSubmit={e => this.submit(e)}>
+          <div className='form-group'>
+            <label htmlFor='email' className='form-control'>
+              Email
+            </label>
+            <input
+              type='email'
+              name='email'
+              id=''
+              onChange={e => this.onChange(e)}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='number'>Number</label>
+            <input
+              type='number'
+              name='number'
+              id=''
+              onChange={e => this.onChange(e)}
+            />
+          </div>
+          <div>
+            <button className='btn text-white'>Submit</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
